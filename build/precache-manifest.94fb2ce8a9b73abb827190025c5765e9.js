de3fc323416299266c8a0f13e959e1e8ec505b8f
Alex <AlexsanderRak>
2020-01-04 17:30:43 +0300

    add build

---
+++ build/precache-manifest.94fb2ce8a9b73abb827190025c5765e9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3c33d68f67d1b46395c671596e10c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4311cba3860c8192dc",
+    "url": "/static/css/main.62c9075e.chunk.css"
+  },
+  {
+    "revision": "ca5d7375b5c1b298238a",
+    "url": "/static/js/2.4a568f38.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4a568f38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d4311cba3860c8192dc",
+    "url": "/static/js/main.f48b0d4a.chunk.js"
+  },
+  {
+    "revision": "e2b08faa304692ed4855",
+    "url": "/static/js/runtime-main.28430c79.js"
+  },
+  {
+    "revision": "ae8171fcd00100e0995d168246ec9a99",
+    "url": "/static/media/burger.ae8171fc.svg"
+  },
+  {
+    "revision": "06a261f33bd29b4079b22948518d15ed",
+    "url": "/static/media/eggs-1577626821823-1090.06a261f3.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "534327f93c0c4de1f0fb7ccd6c98f774",
+    "url": "/static/media/restaurant.534327f9.svg"
+  }
+]);
